e39115aeabef422d6b3914fb44086c564333fbc5
Pam Pelkola <pelkola.pam@gmail.com>
2018-03-02 17:33:09 -0600

    begin javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+<script type="text/javascript">
+</script>
+"use strict"
+
+//variables//
+
+var randomWordArray = [Cinderella, Bengels, Poison];
+var wrongLetterArray = [];
+var guessesLeftCounter = 20;
+var Guesses = []
+var winsCounter = 0;
+var lossesCounter = 0;
+
+var letterArray = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+
+//functions//
+function startGame(){
+randomWordArray[math.floor(math.random() * randomWordArray.length)];
+console.log(randomWordArray);
+}
+
+//main//
+
+for (i = 0; 0<randomWordArray.length; i++){
+console.log(randomWordArray);
+}
