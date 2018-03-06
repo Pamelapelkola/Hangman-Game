fc056080fe4c09beabc82226bfcd1e2367feffd3
Pam Pelkola <pelkola.pam@gmail.com>
2018-03-05 18:16:54 -0600

    final changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,83 @@
-<script type="text/javascript">
-</script>
-"use strict"
+
 
 //variables//
 
-var randomWordArray = [Cinderella, Bengels, Poison];
-var wrongLetterArray = [];
-var guessesLeftCounter = 20;
-var Guesses = []
-var winsCounter = 0;
-var lossesCounter = 0;
+const wordsArray = ['Cinderella', 'Bangles', 'Poison'];
+let winCounter = 0;
+let wrongLetter = [];
+let guessesLeftCounter = 20;
+let guesses = [];
+let underscores = [];
+let newWord =wordsArray[Math.floor(Math.random() * wordsArray.length)];
 
-var letterArray = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
-
-//functions//
+//functions
 function startGame(){
-randomWordArray[math.floor(math.random() * randomWordArray.length)];
-console.log(randomWordArray);
+newWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+
+console.log(newWord);
 }
 
-//main//
-
-for (i = 0; 0<randomWordArray.length; i++){
-console.log(randomWordArray);
+for (let i = 0; i < newWord.length; i++){
+underscores.push('_');
 }
+document.getElementById('placeholder').innerHTML = underscores.join(" ");
+
+wrongLetter = [];
+guessesLeftCounter = 20;
+
+document.getElementById('guesses-left').innerHTML = guessesLeftCounter;
+
+function results(){
+ if (winCounter === newWord.length){
+    alert('winner')
+   startGame()
+  }else(guessesLeftCounter === 0);{
+    alert('you lose');
+    startGame()
+ }
+  
+
+document.onkeyup = function(event){
+guesses = guesses + event.key;
+console.log(event.key)
+document.getElementById("guesses").innerhtml = guesses
+if(newWord.indexOf(guesses) > -1)
+{
+for (var i = 0; i < newWord.length; i++)
+{
+  if(newWord[i] === guesses)
+  {
+  underscores[i] = guesses;
+  console.log(underscores);
+  winCounter ++
+  results()
+  }
+}
+}
+  else (newWord[i] != guesses);{
+  wrongLetter.push(guesses);
+  guessesLeftCounter --;
+  console.log(guessesLeftCounter);
+
+ results()}
+  }
+}
+
+//main
+startGame()
+  
+ // document.onkeyup = function(event)
+ 
+
+  //if(newWord === guesses){
+    //underscores[i] = guesses;
+   // winCounter ++
+  //  result()
+   // return;
+  //}
+  //}
+  //if(newWord != guesses){}
+  //wrongLetter.push(guesses);
+  //guessesLeftCounter --
+ // result()
+
